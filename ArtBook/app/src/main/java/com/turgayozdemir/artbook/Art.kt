1552f0902a657d56ca7bc2ea3b4ac69d56fef676
Turgay Özdemir <ozdemir.turgay@outlook.com.tr>
2024-03-27 13:26:32 +0300

    Art Book project is completed.

---
+++ ArtBook/app/src/main/java/com/turgayozdemir/artbook/Art.kt
@@ -0,0 +1,4 @@
+package com.turgayozdemir.artbook
+
+class Art(val name : String, val id : Int) {
+}
--- ArtBook/app/src/main/java/com/turgayozdemir/artbook/MainActivity.kt
+++ ArtBook/app/src/main/java/com/turgayozdemir/artbook/MainActivity.kt
@@ -1,20 +1,73 @@
 package com.turgayozdemir.artbook
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.turgayozdemir.artbook.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var artList : ArrayList<Art>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        artList = ArrayList<Art>()
+
+        val artAdapter = ArtAdapter(artList)
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.adapter = artAdapter
+
+
+        try {
+            val database = this.openOrCreateDatabase("Arts", MODE_PRIVATE, null)
+
+            val cursor = database.rawQuery("SELECT * FROM arts", null)
+            val arNameIx = cursor.getColumnIndex("artName")
+            val idIx = cursor.getColumnIndex("id")
+
+            while (cursor.moveToNext()){
+                val name = cursor.getString(arNameIx)
+                val id = cursor.getInt(idIx)
+                val art = Art(name, id)
+                artList.add(art)
+            }
+
+            artAdapter.notifyDataSetChanged()
+
+            cursor.close()
+
+        } catch (e : Exception){
+            e.printStackTrace()
         }
+
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val menuInflater = menuInflater
+        menuInflater.inflate(R.menu.art_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.add_art_item){
+            val intent = Intent(this@MainActivity, ArtActivity::class.java)
+            intent.putExtra("info", "new")
+            startActivity(intent)
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+
+
 }
+
