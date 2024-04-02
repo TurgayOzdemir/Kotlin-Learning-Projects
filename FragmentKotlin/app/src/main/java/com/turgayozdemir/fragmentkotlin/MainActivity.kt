95682377740edab57e9a8d94b2ce40c1074bfd34
Turgay Özdemir <ozdemir.turgay@outlook.com.tr>
2024-04-02 15:52:34 +0300

    Fragment Kotlin project is completed.

--- FragmentKotlin/app/src/main/java/com/turgayozdemir/fragmentkotlin/MainActivity.kt
+++ FragmentKotlin/app/src/main/java/com/turgayozdemir/fragmentkotlin/MainActivity.kt
@@ -1,20 +1,40 @@
 package com.turgayozdemir.fragmentkotlin
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.turgayozdemir.fragmentkotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+
+    }
+
+    fun firstButton(view : View){
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+
+        val firstFragment = FirstFragment()
+        fragmentTransaction.replace(R.id.frameLayout, firstFragment).commit()
+    }
+
+    fun secondButton(view : View){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+
+        val secondFragment = SecondFragment()
+        fragmentTransaction.replace(R.id.frameLayout, secondFragment).commit()
     }
 }
