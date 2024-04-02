f3d48cb402d3d91b05499b06f26693968cd7ea3f
Turgay Özdemir <ozdemir.turgay@outlook.com.tr>
2024-04-03 00:47:54 +0300

    Crypto Money initial commit.

---
+++ CryptoMoney/app/src/main/java/com/turgayozdemir/cryptomoney/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.turgayozdemir.cryptomoney
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.turgayozdemir.cryptomoney.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+}
